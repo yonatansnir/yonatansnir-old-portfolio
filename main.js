be3af84329f300135f401cf93cb8a8c29fb396b9
Yonatan Snir <yonatan941@gmail.com>
2020-04-17 10:11:36 +0300

    scroll page fixing

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ var about = document.getElementById("about");
 var abouttext = document.getElementById("about-text");
 var closeabout = document.getElementById("closeabout");
 
+let currentPage = 0;
+let numOfTimes = 0;
+
 function page(num){
-    pages.style.transform = "translateY(" + num + "%)";
+    currentPage = num;
+    let pageNum = num * -100;
+    pages.style.transform = "translateY(" + pageNum + "%)";
     document.getElementById("point-0").classList.remove("point-on");
     document.getElementById("point--100").classList.remove("point-on");
     document.getElementById("point--200").classList.remove("point-on");
-    document.getElementById("point-"+num).classList.toggle("point-on");
+    document.getElementById("point-"+pageNum).classList.toggle("point-on");
 }
 
 function toggleResume(num){
@@ -25,6 +30,36 @@ function toggleResume(num){
     }
 }
 
-window.onscroll(function() {
-    alert('hello')
-})
+pages.addEventListener('wheel', handleWheel);
+
+function handleWheel(e){
+    pages.removeEventListener('wheel', handleWheel);
+    setTimeout(() => pages.addEventListener('wheel', handleWheel), 1500)
+    handleDirection(e.deltaY);
+}
+
+function handleDirection(direction){
+    if (direction > 0){
+        switch (currentPage) {
+            case 0:
+                page(currentPage + 1)
+                break;
+            case 1:
+                page(currentPage + 1);
+                break;
+            default:
+                break;
+        }
+    } else {
+        switch (currentPage) {
+            case 1:
+                page(currentPage - 1)
+                break;
+            case 2:
+                page(currentPage - 1);
+                break;
+            default:
+                break;
+        }
+    }
+}
